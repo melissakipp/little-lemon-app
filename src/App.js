fc28cf6941c7fe25474e6529cc18a8ffa53acbd1
Melissa Kipp <melissakipp.az@gmail.com>
2024-01-11 12:11:04 -0700

    Experimental export and import with Highlight component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import About from './components/main/About';
 import Hero from './components/main/Hero';
-import Highlights from './components/main/Highlights';
+import { Highlights } from './components/main/Highlights';
+// import Highlights from './components/main/Highlights';
 import Testimonials from './components/main/Testimonials';
 
 // Note: these selections are already have a layout of block, it is
