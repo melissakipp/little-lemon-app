ad94e8bf168cfde93ae93b863cc4e9a6b55ef114
Melissa Kipp <melissakipp.az@gmail.com>
2024-01-11 12:13:13 -0700

    Replace standard button with Button component

--- src/components/main/Hero.js
+++ src/components/main/Hero.js
@@ -1,13 +1,20 @@
+import Button from "../ui/buttons/Button";
+// TODO: Future task - include styles at the Button component level
 import "./hero.css";
 
 const Hero = () => {
+
+    const handleClick = () => {
+        console.log('Button was clicked!');
+      };
+
     return (
         <section className="hero-section">
             <div className="col-1">
                 <h2 className="hero-title">Little Lemon</h2>
                 <p className="hero-location">Chicago</p>
                 <p className="hero-descr">Lorem ipsum dolor sit amet consectetur. Mattis turpis potenti cum libero tortor accumsan varius. Placerat tincidunt vitae cursus phasellus.</p>
-                <button>Reservations</button>
+                <Button text="Reservations" onClick={handleClick} />
             </div>
             <div className="col-2">
                 <img className="hero-image" src="/images/restauranfood.jpg" alt="restaurant food" />
