6aa7ef54a90bfda9791c69a130b3513cfb928bef
Melissa Kipp <melissakipp.az@gmail.com>
2024-01-11 12:16:39 -0700

    Add basic card structure

--- src/components/main/Highlights.js
+++ src/components/main/Highlights.js
@@ -1,9 +1,15 @@
-function Highlights() {
+export const Highlights = () => {
     return (
-        <div>
+        <>
             <h2>This weeks specials!</h2>
-        </div>
+            {/* create three cards with an image on top, title, price, and description */}
+            <div className="card">
+                <img src="https://via.placeholder.com/150" alt="placeholder" />
+                <h3>Product 1</h3>
+                <p>$10.00</p>
+                <p>Product description</p>
+                <p>Delivery Available</p>
+            </div>
+        </>
     );
 }
-
-export default Highlights;
