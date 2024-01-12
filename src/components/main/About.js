7449bbbc35a4f4ad153a93b5f69a866471c37df3
Melissa Kipp <melissakipp.az@gmail.com>
2024-01-12 07:26:37 -0700

    Basic structure for about section and clean up in main.css

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -1,8 +1,16 @@
 const About = () => {
     return (
-        <div>
-            <h2>About Section</h2>
-        </div>
+        <section className="about-section">
+            <div className="col-1">
+                <h2 className="about-title">Little Lemon Story</h2>
+                <p className="hero-location">Chicago</p>
+                <p className="hero-descr">Lorem ipsum dolor sit amet consectetur. Mattis turpis potenti cum libero tortor accumsan varius. Placerat tincidunt vitae cursus phasellus. Lorem ipsum dolor sit amet consectetur. Mattis turpis potenti cum libero tortor accumsan varius. Placerat tincidunt vitae cursus phasellus. Lorem ipsum dolor sit amet consectetur. Mattis turpis potenti cum libero tortor accumsan varius. Placerat tincidunt vitae cursus phasellus.</p>
+            </div>
+            <div className="col-2">
+                <img className="hero-image" src="https://via.placeholder.com/300x400" alt="restaurant food" />
+                <img className="hero-image" src="https://via.placeholder.com/300x400" alt="restaurant food" />
+            </div>
+        </section>
     );
 };
 
