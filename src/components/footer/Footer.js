c8261a151ffea17b2d960aefac0f142f98a2a741
Melissa Kipp <melissakipp.az@gmail.com>
2024-01-12 10:15:08 -0700

    Basic footer structure

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import './footer.css';
 
 const Footer = () => {
     return (
-        <footer>
-            <h2 className='sr-only'>Site Footer</h2>
-            <section>
-                <h3>Site Menu</h3>
-                <ul>
-                    <li><a href='/'>Home</a></li>
-                    <li><a href='/about'>About</a></li>
-                    <li><a href='/menu'>Menu</a></li>
-                    <li><a href='/reservation'>Reservation</a></li>
-                    <li><a href='/online-order'>Online order</a></li>
-                    <li><a href='/login'>Login</a></li>
-                </ul>
-            </section>
-            <section>
-                <h3>Contact</h3>
-                <address>
-                    <a href="mailto:info@littlelemon.com">info@littlelemon.com</a><br />
-                    <a href="tel:+14800000000">(480) 000-0000</a>
-                </address>
-            </section>
-            <section>
-                <h3>Social Media</h3>
-            </section>
+        <footer className='footer-section'>
+            <article className='footer-grid'>
+                <h2 className='sr-only'>Site Footer</h2>
+                <section>
+                    <img src='https://via.placeholder.com/150x200' alt='logo' />
+                </section>
+                <section>
+                    <h3 className='footer-titles'>Site Menu</h3>
+                    <ul>
+                        <li><a href='/'>Home</a></li>
+                        <li><a href='/about'>About</a></li>
+                        <li><a href='/menu'>Menu</a></li>
+                        <li><a href='/reservation'>Reservation</a></li>
+                        <li><a href='/online-order'>Online order</a></li>
+                        <li><a href='/login'>Login</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <h3 className='footer-titles'>Contact</h3>
+                    <address>
+                        <a href="mailto:info@littlelemon.com">info@littlelemon.com</a><br />
+                        <a href="tel:+14800000000">(480) 000-0000</a>
+                    </address>
+                </section>
+                <section>
+                    <h3 className='footer-titles'>Social Media</h3>
+                    <ul>
+                        <li><a href='https://www.facebook.com'>Facebook</a></li>
+                        <li><a href='https://www.twitter.com'>Twitter</a></li>
+                        <li><a href='https://www.instagram'>Instagram</a></li>
+                    </ul>
+                </section>
+            </article>
         </footer>
     );
 };
