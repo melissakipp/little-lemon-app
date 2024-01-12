19ea31f985a39f3095d2ea594dee518c14127918
Melissa Kipp <melissakipp.az@gmail.com>
2024-01-12 07:08:25 -0700

    Basic structure for testimonials section

--- src/components/main/Testimonials.js
+++ src/components/main/Testimonials.js
@@ -1,8 +1,46 @@
 const Testimonials = () => {
     return (
-        <div>
-            <h2>Testimonials</h2>
-        </div>
+        <section className="testimonials-section">
+            <h2 className='section-title'>Testimonials</h2>
+            <ul className='testimonials'>
+                <li className='user'>
+                    <p className='user-name'>
+                        <strong>John Doe</strong>
+                    </p>
+                    <img className='user-img' src='https://via.placeholder.com/75' alt='user' />
+                    <p className='user-review'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, quidem. Quibusdam, quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, quidem. Quibusdam, quidem.
+                    </p>
+                </li>
+                <li className='user'>
+                    <p className='user-name'>
+                        <strong>John Doe</strong>
+                    </p>
+                    <img className='user-img' src='https://via.placeholder.com/75' alt='user' />
+                    <p className='user-review'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, quidem. Quibusdam, quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, quidem. Quibusdam, quidem.
+                    </p>
+                </li>
+                <li className='user'>
+                    <p className='user-name'>
+                        <strong>John Doe</strong>
+                    </p>
+                    <img className='user-img' src='https://via.placeholder.com/75' alt='user' />
+                    <p className='user-review'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, quidem. Quibusdam, quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, quidem. Quibusdam, quidem.
+                    </p>
+                </li>
+                <li className='user'>
+                    <p className='user-name'>
+                        <strong>John Doe</strong>
+                    </p>
+                    <img className='user-img' src='https://via.placeholder.com/75' alt='user' />
+                    <p className='user-review'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, quidem. Quibusdam, quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, quidem. Quibusdam, quidem.
+                    </p>
+                </li>
+            </ul>
+        </section>
     );
 };
 
