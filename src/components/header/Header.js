78347904d542a39206d5a35a9e0d4501aa51f45b
Melissa Kipp <melissakipp.az@gmail.com>
2024-01-13 16:06:56 -0700

    Basic main nav structure setup

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,4 @@
+import { MainNavigation } from "./Main-Navigation";
 import "./header.css";
 
 const Header = () => {
@@ -7,21 +8,12 @@ const Header = () => {
                 <h1 className="sr-only">Little Lemon Homepage</h1>
                 <img className="logo" src="/images/Logo.svg" alt="Little Lemon logo" />
             </div>
+            <MainNavigation />
             <div className="burger-menu">
                 <svg width="28" height="18" viewBox="0 0 35 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fillRule="evenodd" clipRule="evenodd" d="M1.34615 0C0.6027 0 0 0.621833 0 1.38889C0 2.15594 0.6027 2.77778 1.34615 2.77778H33.6538C34.3972 2.77778 35 2.15594 35 1.38889C35 0.621833 34.3972 0 33.6538 0H1.34615ZM0 12.5C0 11.7329 0.6027 11.1111 1.34615 11.1111H33.6538C34.3972 11.1111 35 11.7329 35 12.5C35 13.2671 34.3972 13.8889 33.6538 13.8889H1.34615C0.6027 13.8889 0 13.2671 0 12.5ZM0 23.6111C0 22.8442 0.6027 22.2222 1.34615 22.2222H33.6538C34.3972 22.2222 35 22.8442 35 23.6111C35 24.3781 34.3972 25 33.6538 25H1.34615C0.6027 25 0 24.3781 0 23.6111Z" fill="black"/>
                 </svg>
             </div>
-            <nav className="navbar">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/menu">Menu</a></li>
-                    <li><a href="/reservation">Reservation</a></li>
-                    <li><a href="/order-online">Order online</a></li>
-                    <li><a href="/login">Login</a></li>
-                </ul>
-            </nav>
         </header>
     );
 };
---
+++ src/components/header/Main-Navigation.js
@@ -0,0 +1,16 @@
+export const MainNavigation = () => {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/menu">Menu</a></li>
+                <li><a href="/reservation">Reservation</a></li>
+                <li><a href="/order-online">Order online</a></li>
+                <li><a href="/login">Login</a></li>
+            </ul>
+        </nav>
+
+    );
+};
+
