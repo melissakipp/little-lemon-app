ed75faab88d3426e9a368cce21ace3e88db94491
Melissa Kipp <melissakipp.az@gmail.com>
2024-01-25 09:10:12 -0700

    Add color proper to Button

--- src/pages/homepage/Hero.js
+++ src/pages/homepage/Hero.js
@@ -1,6 +1,4 @@
-import Button from "../../components/ui/button/Button";
-
-// TODO: Future task - include styles at the Button component level
+import Button from '../../components/ui/button/Button';
 
 const Hero = () => {
 
@@ -9,15 +7,15 @@ const Hero = () => {
       };
 
     return (
-        <section className="hero-section">
-            <div className="col-1">
-                <h2 className="hero-title">Little Lemon</h2>
-                <p className="hero-location">Chicago</p>
-                <p className="hero-descr">Lorem ipsum dolor sit amet consectetur. Mattis turpis potenti cum libero tortor accumsan varius. Placerat tincidunt vitae cursus phasellus.</p>
-                <Button text="Reservations" onClick={handleClick} />
+        <section className='hero-section'>
+            <div className='col-1'>
+                <h2 className='hero-title'>Little Lemon</h2>
+                <p className='hero-location'>Chicago</p>
+                <p className='hero-descr'>Lorem ipsum dolor sit amet consectetur. Mattis turpis potenti cum libero tortor accumsan varius. Placerat tincidunt vitae cursus phasellus.</p>
+                <Button color='ctaButtonColor' text='Reservations' onClick={handleClick} />
             </div>
-            <div className="col-2">
-                <img className="hero-image" src="/images/restauranfood.jpg" alt="restaurant food" />
+            <div className='col-2'>
+                <img className='hero-image' src='/images/restauranfood.jpg' alt='restaurant food' />
             </div>
         </section>
     );
