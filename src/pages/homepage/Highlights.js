9749c565e1effa96ecaefeeefa00a88811e76e21
Melissa Kipp <melissakipp.az@gmail.com>
2024-01-25 19:32:30 -0700

    Enhance button features

--- src/pages/homepage/Highlights.js
+++ src/pages/homepage/Highlights.js
@@ -1,10 +1,12 @@
 import Card from '../../components/ui/card/Card';
+import Button from '../../components/ui/button/Button';
 
 export default function Highlights() {
     return (
         <section className='highlights-section'>
             <div className='announcement'>
                 <h2>This weeks specials!</h2>
+                <Button color='orderButtonColor' text="Order online" />  
             </div>
             {/* create three cards with an image on top, title, price, and description */}
             <div className='card-gallary'>
