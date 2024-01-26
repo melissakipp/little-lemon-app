47dda954a482741d27a1cb7bb09674690dca728e
Melissa Kipp <melissakipp.az@gmail.com>
2024-01-26 11:31:53 -0700

    Final about section

--- src/pages/homepage/About.js
+++ src/pages/homepage/About.js
@@ -1,17 +1,18 @@
-const About = () => {
+import PictureOne from '../../assets/storefront.png';
+import PictureTwo from '../../assets/dining-in.png';
+
+export default function About() {
     return (
-        <section className="about-section">
-            <div className="col-1">
-                <h2 className="about-title">Little Lemon Story</h2>
-                <p className="hero-location">Chicago</p>
-                <p className="hero-descr">Lorem ipsum dolor sit amet consectetur. Mattis turpis potenti cum libero tortor accumsan varius. Placerat tincidunt vitae cursus phasellus. Lorem ipsum dolor sit amet consectetur. Mattis turpis potenti cum libero tortor accumsan varius. Placerat tincidunt vitae cursus phasellus. Lorem ipsum dolor sit amet consectetur. Mattis turpis potenti cum libero tortor accumsan varius. Placerat tincidunt vitae cursus phasellus.</p>
+        <section className='about-section'>
+            <div className='about-info'>
+                <h2 className='about-title'>Little Lemon Story</h2>
+                <p className='hero-location'>Chicago</p>
+                <p className='hero-descr'>Lorem ipsum dolor sit amet consectetur. Mattis turpis potenti cum libero tortor accumsan varius. Placerat tincidunt vitae cursus phasellus. Lorem ipsum dolor sit amet consectetur. Mattis turpis potenti cum libero tortor accumsan varius. Placerat tincidunt vitae cursus phasellus. Lorem ipsum dolor sit amet consectetur. Mattis turpis potenti cum libero tortor accumsan varius. Placerat tincidunt vitae cursus phasellus.</p>
             </div>
-            <div className="col-2">
-                <img className="hero-image" src="https://via.placeholder.com/300x400" alt="restaurant food" />
-                <img className="hero-image" src="https://via.placeholder.com/300x400" alt="restaurant food" />
+            <div className='about-images'>
+                <img className='overlap-1' src={PictureOne} alt='Our first location.' />
+                <img className='overlap-2' src={PictureTwo} alt='A woman enjoying our dine-in experience.' />
             </div>
         </section>
     );
-};
-
-export default About;
+}
