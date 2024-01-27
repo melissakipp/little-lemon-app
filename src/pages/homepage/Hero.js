ec59aa4add8dd6f89f9b19f1feb11ce0fadf85d1
Melissa Kipp <melissakipp.az@gmail.com>
2024-01-26 17:52:04 -0700

    Fix reservation button

--- src/pages/homepage/Hero.js
+++ src/pages/homepage/Hero.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import HeroImage from '../../assets/restaurant-food.jpg';
 import Button from '../../components/ui/button/Button';
 
@@ -13,7 +15,9 @@ const Hero = () => {
                 <h2 className='hero-title'>Little Lemon</h2>
                 <p className='hero-location'>Chicago</p>
                 <p className='hero-descr'>Lorem ipsum dolor sit amet consectetur. Mattis turpis potenti cum libero tortor accumsan varius. Placerat tincidunt vitae cursus phasellus.</p>
-                <Button color='ctaButtonColor' text='Reservations' onClick={handleClick} />
+                <Link to='/reservation'>
+                    <Button color='ctaButtonColor' text='Reservations' onClick={handleClick} />
+                </Link>
             </div>
             <div className='col-2'>
                 <img className='hero-image' src={HeroImage} alt='restaurant food' />
